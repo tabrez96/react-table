3db1c0c29c9079d71e363283b5734529141ca57c
Tabrez Basha <tab22rez@gmail.com>
2019-06-18 23:47:08 +0530

    Add and configure webpack and webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'file-loader?limit=8192&name=assets/[name].[ext]?[hash]'
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    inline:true,
+    port: 4200
+  },
+};
